062d97bb309d2bc2aebbe24cb8bc2ca06f4c4eff
wangfangjie <wangfangjie@pdmi.cn>
2017-05-05 19:36:44 +0800

    demo calc loan

--- app/main/views.py
+++ app/main/views.py
@@ -7,10 +7,18 @@ Version:        0.1.0
 FileName:       views.py
 CreateTime:     2017-05-02 22:00
 """
-from flask import render_template
+import json
+from flask import render_template, request
 from ..import db
 from . import main
 
 @main.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@main.route('/calculate-loan', methods = ['POST'])
+def calculate_loan():
+    print request.form
+    import time
+    time.sleep(2)
+    return json.dumps({'result':"ok"})
